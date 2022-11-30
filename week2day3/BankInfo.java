5be6c2226da02154947bab4a3e4bdc7ff7ab597d
amrushadaka <118620155+amrushadaka@users.noreply.github.com>
2022-11-30 18:38:55 +0530

    Add files via upload

---
+++ week2day3/BankInfo.java
@@ -0,0 +1,15 @@
+package week2day3;
+
+public class BankInfo {
+
+
+	public void saving() {
+		System.out.println("saving the money");
+	}
+	public void fixed() {
+		System.out.println("in fixed"); 
+	}
+	public void deposit() {
+		System.out.println("deposit");
+	}
+}
