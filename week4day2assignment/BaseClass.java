214940376c57698b433fb7f03c2f0c330f014018
amrushadaka <118620155+amrushadaka@users.noreply.github.com>
2022-12-13 23:13:47 +0530

    Add files via upload
    week4day2assignment

---
+++ week4day2assignment/BaseClass.java
@@ -0,0 +1,50 @@
+package week4day2assignment;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import java.time.Duration; 
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseClass {
+RemoteWebDriver driver;
+	
+	@Parameters({"browser","url","username","password"})
+	
+	@BeforeMethod 
+	public void preCondition(String browser,String url,String username,String password )
+	{
+		if (browser.equalsIgnoreCase("chrome"))
+		{
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		}
+		else if(browser.equalsIgnoreCase("edge"))
+		{
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}
+
+		// Launch Sales force application
+		driver.get(url);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		// Logging with credentials
+		driver.findElement(By.xpath("//input[@id='username']")).sendKeys(username);
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys(password);
+		driver.findElement(By.xpath("//input[@id='Login']")).click();
+	}
+
+	@AfterMethod
+	public void postCondition()
+	{
+		driver.close();
+
+}
+}
